c7d4f8480fb7c38ec6b726b311be5f725c37ee88
sigcho <69459198+sigcho@users.noreply.github.com>
2020-08-26 21:29:06 +0900

    PHP_JS_함수의 입력과 출력_input동일명_return
    input(다른 어느말도 됨)을 동일하게 넣어서 입력값이 나오게한다.
    return 을 입력해서 출력을 한다.

---
+++ 13.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <script>
+      function a(input){
+        return input+1;
+      }
+      document.write(a(5));
+      prompt(a(5));
+    </script>
+    <h1>php</h1>
+    <?php
+      function a($input){
+        return $input+1;
+      }
+      echo(a(5));
+    ?>
+  </body>
+</html>
